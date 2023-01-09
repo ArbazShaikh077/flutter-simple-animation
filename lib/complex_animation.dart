69b0c950a6303cce0f594ae633e5d118886fb3ba
Arbaz Shaikh <shaikharbaz077@gmail.com>
2023-01-09 23:09:36 +0530

    added complex animation

---
+++ lib/complex_animation.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class FadeInAnimation extends StatefulWidget {
+  const FadeInAnimation({super.key});
+
+  @override
+  State<FadeInAnimation> createState() => _FadeInAnimationState();
+}
+
+class _FadeInAnimationState extends State<FadeInAnimation>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller!);
+  }
+
+  @override
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _animation!,
+      child: const Text('Hello World!'),
+    );
+  }
+}
